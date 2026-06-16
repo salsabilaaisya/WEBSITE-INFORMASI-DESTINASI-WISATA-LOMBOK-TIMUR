b1da8eae6acf48efa64bef8a1ad2c3e8dae1b54e
salsabilaaisya <salsabilaaisyazulhijjah.240604051@student.hamzanwadi.ac.id>
2026-06-16 14:51:09 +0800

    initial commit

---
+++ resources/views/pages/category/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h1 class="text-2xl font-bold text-red-500">Halo bosque</h1>
+    {{-- Life is available only in the present moment. - Thich Nhat Hanh --}}
+</div>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome')->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('categories', 'pages::category.index')->name('category.index');
+});
+
+require __DIR__.'/settings.php';
