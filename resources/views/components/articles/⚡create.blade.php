aac98417f032f47f30bfd8ddb45a74a218750342
balinux <ryojuniyantara@gmail.com>
2026-06-20 07:37:32 +0800

    update: page article

--- resources/views/components/articles/⚡create.blade.php
+++ resources/views/components/articles/⚡create.blade.php
@@ -1,26 +1,53 @@
 <?php
 
 use Livewire\Component;
+use App\Livewire\Forms\ArticlesForm;
 
-new class extends Component
-{
+new class extends Component {
+    public ArticlesForm $form;
     //
+    public function save()
+    {
+        $this->form->store();
+    }
 };
 ?>
 
 <div>
-    <flux:modal name="edit-profile" class="md:w-96">
-    <div class="space-y-6">
-        <div>
-            <flux:heading size="lg">Update profile</flux:heading>
-            <flux:text class="mt-2">Make changes to your personal details.</flux:text>
-        </div>
-        <flux:input label="Name" placeholder="Your name" />
-        <flux:input label="Date of birth" type="date" />
-        <div class="flex">
-            <flux:spacer />
-            <flux:button type="submit" variant="primary">Save changes</flux:button>
-        </div>
-    </div>
-</flux:modal>
+    <flux:modal name="create-article" class="md:w-150">
+        <form class="space-y-8" wire:submit.prevent="save">
+            {{-- header --}}
+            <div class="space-y-2">
+                <flux:heading size="lg" class="text-zinc-900 dark:text-white">
+                    Create Articles
+                </flux:heading>
+                <flux:text class="text-zinc-500 dark:text-zinc-400">
+                    Add a new article to your account
+                </flux:text>
+            </div>
+
+            {{-- form field --}}
+            <div class="space-y-6">
+                <flux:input label="Title" placeholder="Enter articles title" wire:model="form.title" />
+
+                <flux:textarea label="Content" placeholder="Enter articles content" wire:model="form.content" />
+
+                <flux:input label="Thumbnail" placeholder="Enter thumbnail filename" wire:model="form.thumbnail" />
+
+                <flux:input label="User ID" type="number" placeholder="Enter user id" wire:model="form.user_id" />
+
+                <flux:input label="Published At" type="date" wire:model="form.published_at" />
+            </div>
+
+            {{-- footer --}}
+            <div class="flex items-center justify-end gap-3 pt-4 border-t border-zinc-200 dark:border-zinc-800">
+                <flux:modal.close>
+                    <flux:button variant="outline" color="neutral">Cancel</flux:button>
+                </flux:modal.close>
+                <flux:button variant="primary" color="primary" type="submit">Create</flux:button>
+            </div>
+
+
+        </form>
+    </flux:modal>
 </div>
