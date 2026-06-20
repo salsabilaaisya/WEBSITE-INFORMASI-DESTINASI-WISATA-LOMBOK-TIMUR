98d3d8626ca7f97194e3583f225b870a7026631a
salsabilaaisya <salsabilaaisyazulhijjah.240604051@student.hamzanwadi.ac.id>
2026-06-21 05:23:48 +0800

    Update: web.php n yang sblumnya itu update destination tapi kelupaan pesannya tadi ehe

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth'])->group(function () {
@@ -22,17 +20,12 @@ Route::livewire('/destinations', 'pages::destination.index')
     ->name('destination.index')
     ->middleware(['auth']);
 
-<<<<<<< HEAD
 Route::livewire('/destinations/create', 'pages::destination.form')
     ->name('destination.form')
     ->middleware(['auth']);
 
-Route::livewire('/destination/create', 'pages::DestinationIndex::class')
-    ->name('destination.index')
-=======
 Route::livewire('/gallery', 'pages::gallery.index')
     ->name('gallery.index')
->>>>>>> a7ca6d5bd0781f8ae45474eee0d78874e1cce138
     ->middleware(['auth']);
 
 require __DIR__ . '/settings.php';
